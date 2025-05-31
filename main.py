7475095b8757c9b753444d72fbe1cb336b6241a3
Vincent Jose <vincentjosenathanael@gmail.com>
2025-05-31 13:58:04 +0700

    Fixed The bug

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main():
     high_number = 100
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
-    print("You have 5 chances to guess the correct number.\n")
+    print("If you guess the number out of range, your chances will be reduced.")
+    print("You can guess the number in 3 different difficulty levels.\n")
     print("Please select the difficulty level:")
     print("1. Easy (10 chances)")
     print("2. Medium (5 chances)")
@@ -32,9 +33,11 @@ def main():
                     break
                 case _:
                     print("Please enter a valid level!")
+                    continue
         except ValueError:
             print("Please enter a number!")
-        print("Let's start the game!\n")
+            continue
+    print("Let's start the game!\n")
     while isrunning:
         guess = input(f"Guess the number (between {low_number} and {high_number}) - Chances left: {chances} : ")
         if not guess.isdigit():
@@ -43,8 +46,9 @@ def main():
         guess = int(guess)
         if guess < low_number or guess > high_number:
             print(f"Please enter a number between {low_number} and {high_number}!")
+            chances -= 1
             continue
-        if guess == secret_number:
+        elif guess == secret_number:
             print(f"Congratulations! You guessed the number {secret_number} correctly!")
             if chances > highscore:
                 highscore = chances
@@ -55,16 +59,13 @@ def main():
             if chances == 0:
                 print(f"Sorry, you've run out of chances. The secret number was {secret_number}.")
                 break
-            elif guess < secret_number:
+            if guess < secret_number:
                 print("Too low! Try again.")
-                if guess > low_number or low_number == 1:
-                    low_number = guess + 1
             else:
-                print("Too high! Try again.")  
-                if guess < high_number or high_number == 100: 
-                    high_number = guess - 1
-            
-  
-             
+                print("Too high! Try again.") 
+        if guess < secret_number and guess > low_number:
+            low_number = guess + 1 
+        elif guess > secret_number and guess < high_number:
+            high_number = guess - 1
 if __name__ == "__main__":
     main()
